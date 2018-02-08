2c11374610ed8144f985bde363c27f471ef5e767
Thomas Palzkill <tpalzkilll@gmail.com>
2018-02-08 09:54:01 -0600

    xcode project created

---
+++ Rad-Crawls/Rad-CrawlsTests/Rad_CrawlsTests.swift
@@ -0,0 +1,36 @@
+//
+//  Rad_CrawlsTests.swift
+//  Rad-CrawlsTests
+//
+//  Created by Thomas  Palzkill on 2/8/18.
+//  Copyright © 2018 Thomas  Palzkill. All rights reserved.
+//
+
+import XCTest
+@testable import Rad_Crawls
+
+class Rad_CrawlsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
